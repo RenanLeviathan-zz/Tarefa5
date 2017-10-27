8e176ad876c632ee56b282b8dd3b606847a85f3c
RenanLeviathan <renan.leviathan@gmail.com>
2017-10-26 22:22:01 -0300

    programme terminé

--- main.py
+++ main.py
@@ -4,40 +4,38 @@ Created on Tue Oct 17 16:09:36 2017
 
 @author: Israël e Renan
 """
-#modulo de lista de adjacências
-def lista_adj(vertices):
-    lista={}
-    for i in vertices:
-        v=[int(x) for x in input("adjacentes a {}\n".format(i)).split()]
-        lista[i]=v
-    return lista
-        
-def mostrar_lista(lista):
-    for i in lista:
-        print(i)
-        
-def aresta_explorada(l,marc):
-    exp=[]
-    for i in l.keys():
-        if i not in marc:
-            print("marca {}".format(i))
-            marc.append(i)
-        partida=i
-        for w in l[partida]:
-            if w not in marc:
-                print("marca {}".format(w))
-                marc.append(w)
-            if (w in l[partida]) and (((partida,w) not in exp) and ((w,partida) not in exp)): 
-                print("explora a aresta ({},{})".format(partida,w))
-                exp.append((partida,w))
-                partida=w
-    return exp
+class Grafo:
+    def __init__(self,vertices,inicial):
+        self.lista={}
+        self.marc=[]
+        self.queue=[]
+        self.marc.append(inicial)
+        self.queue.append(inicial)
+        for i in vertices:
+            v=[x for x in input("adjacentes a {}\n".format(i)).split()]
+            self.lista[i]=v
+    
+    def mostrar(self):
+        for i in self.lista:
+            print(i)
+    
+    def busca_largura(self):
+        exp=[]
+        while self.queue != []:
+            v=self.queue[0]
+            self.queue=self.queue[1:]
+            for w in self.lista[v]:
+                if w not in self.marc:
+                    exp.append((v,w))
+                    self.queue.append(w)
+                    self.marc.append(w)
+                else:
+                    if ((v,w) not in exp) and ((w,v) not in exp):
+                        exp.append((v,w))
+        return exp
                     
-                
-            
-vertices=[int(x) for x in input("Vértices do grafo").split()]
-lista=lista_adj(vertices)
-marcados=[]#lista dos vértices marcados
-exp=aresta_explorada(lista,marcados)
-print(lista)
+vertices=[x for x in input("Vértices do grafo").split()]
+inicial=input("Vértice inicial:")
+g=Grafo(vertices,inicial)
+exp=g.busca_largura()
 print([i for i in exp])
